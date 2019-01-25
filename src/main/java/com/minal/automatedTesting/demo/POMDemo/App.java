54104f61f960c1c37bc0561294e583644697b589
joshsaul1996 <joshsaul1996@gmail.com>
2019-01-25 13:46:01 +0000

    POM exampls and exercise

---
+++ src/main/java/com/minal/automatedTesting/demo/POMDemo/App.java
@@ -0,0 +1,13 @@
+package com.minal.automatedTesting.demo.POMDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
